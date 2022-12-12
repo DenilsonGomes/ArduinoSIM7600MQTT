eb40a2cbcfd426ff8e2d9f6e7b850008a9c07652
Denilson Gomes <denilsongomesdg97@gmail.com>
2022-12-12 09:54:18 -0300

    Comandos e prints atualizados

--- ArduinoSIM7600MQTT.ino
+++ ArduinoSIM7600MQTT.ino
@@ -5,7 +5,7 @@
  */
 
 // Bibliotecas
-#include<SoftwareSerial.h> // Biblioteca SoftwareSerial
+#include <SoftwareSerial.h> // Biblioteca SoftwareSerial
 
 // Variáveis
 int tensaoA; // Variavel para leitura analógica
@@ -13,7 +13,7 @@ String aux1; // String auxiliar1
 String aux2; // String auxiliar2
 
 // Objeto
-SoftwareSerial mySerial(3,2); // Objeto mySerial
+SoftwareSerial mySerial(2,3); // Objeto mySerial
 
 // Setup
 void setup() {
@@ -24,35 +24,36 @@ void setup() {
 
 void loop() {
   tensaoA = analogRead(A0); // Leitura da tensão A
-  if(tensaoA > 800){ // Caso seja necessário enviar a leitura por MQTT
+  if(1){ // Caso seja necessário enviar a leitura por MQTT
+    Serial.println("Entrou no if");
     mySerial.println("ATE0"); // Inicia o modem
-    delay(2000);
+    delay(3000);
     mySerial.println("AT+CMQTTSTART"); // Inicia conexão MQTT
-    delay(2000); 
+    delay(3000); 
     mySerial.println("AT+CMQTTACCQ=0,\"Client c\""); // Client ID
-    delay(2000);
-    mySerial.println("AT+CMQTTCONNECT=0,\"tcp://demo.thingsboard.io:1883\",90,1,\"qAGigVtsUtsWuuCK0q8u\""); // Conecta ao Servidor MQTT
-    delay(2000);
+    delay(3000);
+    mySerial.println("AT+CMQTTCONNECT=0,\"tcp://demo.thingsboard.io:1883\",90,1,\"EKx0Da1Zjiue6N9XkVLf\""); // Conecta ao Servidor MQTT
+    delay(3000);
     mySerial.println("AT+CMQTTTOPIC=0,23"); // Tamanho do nome do topico
-    delay(2000);
+    delay(3000);
     mySerial.println("v1/devices/me/telemetry"); // Topico
-    delay(2000);
+    delay(3000);
     aux1 = "{\"B\":" + String(tensaoA) + "}"; // payload
     aux2 = "AT+CMQTTPAYLOAD=0," + String(aux1.length());
     mySerial.println(aux2); // Define tamanho da payload
-    delay(2000);
+    delay(3000);
     mySerial.println(aux1); // Payload
-    delay(2000);
+    delay(3000);
     mySerial.println("AT+CMQTTPUB=0,1,60"); // Publica a payload
-    delay(2000);
+    delay(3000);
     mySerial.println("AT+CMQTTDISC=0,60"); // Disconecta o cliente
-    delay(2000);
+    delay(3000);
     mySerial.println("AT+CMQTTREL=0"); // Release o servidor
-    delay(2000);
+    delay(3000);
     mySerial.println("AT+CMQTTSTOP"); // Encerra conexão MQTT
-    Serial.println(aux1);
     Serial.println(aux2);
+    Serial.println(aux1);
   }
-  Serial.println(tensaoA);
+//  Serial.println(tensaoA);
   delay(10000);
 }
